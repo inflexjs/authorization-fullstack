578f5c82bba938f9580d5e7f12e9e9df467de220
inflexible <inflexgg@vk.com>
2022-07-01 01:34:17 +0300

    vps fixes

--- client/src/js/http/index.ts
+++ client/src/js/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {AuthResponse} from "@/js/types/AuthResponse";
 
-export const API_URL = `http://86.110.212.55:5000/api`
+export const API_URL = `http://inflexible.ru:8087/api`
 
 const $api = axios.create({
     withCredentials: true,

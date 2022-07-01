bf95cddb65f9c8b60a813239a7adcfe174149523
inflex <inflex_off@vk.com>
2022-07-01 11:41:54 +0300

    fix env

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import router from './router/index.js'
 import errorMiddleware from "./middlewares/error.middleware.js";
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8087
 const app = express()
 
 app.use(express.json())

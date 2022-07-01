e320e5e35fe3fbfea1f4fca165867c88d357cbf1
inflex <inflex_off@vk.com>
2022-07-01 13:22:59 +0300

    remove console logs

--- server/middlewares/activated.middleware.js
+++ server/middlewares/activated.middleware.js
@@ -3,7 +3,7 @@ import TokenService from "../services/token.service.js";
 
 export default function (req, res, next) {
 	try {
-		console.log(req, req, res)
+		// console.log(req, req, res)
 		next()
 	} catch (e) {
 		return next(ApiError.ForbiddenError())

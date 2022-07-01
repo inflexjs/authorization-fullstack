91c804aa7d8e585cb51380e8e865029d99794e5b
inflex <inflex_off@vk.com>
2022-07-02 01:02:41 +0300

    https server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv'
+import https from 'https'
+import * as fs from "fs";
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -10,11 +12,16 @@ dotenv.config()
 const PORT = process.env.PORT || 8087
 const app = express()
 
+const options = {
+	cert: fs.readFileSync('./sslcert/fullchain.pem'),
+	key: fs.readFileSync('./sslcert/privkey.pem')
+};
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
 	credentials: true,
-	origin: ['http://inflexible.ru', 'https://inflexible.ru']
+	origin: process.env.APP_URL
 }))
 app.use('/api', router)
 app.use(errorMiddleware)
@@ -23,6 +30,7 @@ const start = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_DB_URL)
 		app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+		https.createServer(options, app).listen(8443)
 	} catch (e) {
 		console.log(e)
 	}

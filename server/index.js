00e9261924c73c2918fb5bd390a8be75f4a6d4c7
inflex <inflex_off@vk.com>
2022-07-02 12:13:42 +0300

    remove https npm

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 import dotenv from 'dotenv'
-import https from 'https'
-import * as fs from "fs";
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -11,11 +9,6 @@ dotenv.config()
 
 const PORT = process.env.PORT || 8087
 const app = express()
-
-const options = {
-	cert: fs.readFileSync('./sslcert/fullchain.pem'),
-	key: fs.readFileSync('./sslcert/privkey.pem')
-};
 
 app.use(express.json())
 app.use(cookieParser())

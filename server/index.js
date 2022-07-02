eb80a71f4e1513be4c945cdb25ec6d1d90d9c42c
inflex <inflex_off@vk.com>
2022-07-02 13:27:37 +0300

    minor

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 import dotenv from 'dotenv'
-import https from 'https'
-import * as fs from "fs";
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -12,14 +10,12 @@ dotenv.config()
 const PORT = process.env.PORT || 8087
 const app = express()
 
-const options = {
-	cert: fs.readFileSync('./sslcert/fullchain.pem'),
-	key: fs.readFileSync('./sslcert/privkey.pem')
-};
-
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+	credentials: true,
+	origin: process.env.APP_URL
+}))
 app.use('/api', router)
 app.use(errorMiddleware)
 
@@ -27,7 +23,6 @@ const start = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_DB_URL)
 		app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-		https.createServer(options, app).listen(8443)
 	} catch (e) {
 		console.log(e)
 	}
